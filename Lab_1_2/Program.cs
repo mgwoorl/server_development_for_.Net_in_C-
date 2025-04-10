6076b8c374cb29cd47324446d89dc8a387a9e390
mgwoorl <149603747+mgwoorl@users.noreply.github.com>
2025-04-10 11:03:46 +0800

    Update Program.cs

--- Lab_1_2/Program.cs
+++ Lab_1_2/Program.cs
@@ -1,28 +1,31 @@
 using System;
 using System.Threading;
 
-class Program
+namespace Lab_1_2
 {
-    static void Main()
+    class Program
     {
-        Thread firstThread = new Thread(PrintNumbers);
-        Thread secondThread = new Thread(PrintNumbers);
-
-        firstThread.Start();
-        firstThread.Join();
-
-        Thread.Sleep(1000);
-
-        secondThread.Start();
-        secondThread.Join();
-    }
-
-    static void PrintNumbers()
-    {
-        for (int i = 1; i <= 100; i++)
+        static void Main()
         {
-            Console.WriteLine(i);
-            Thread.Sleep(100);
+            Thread firstThread = new Thread(PrintNumbers);
+            Thread secondThread = new Thread(PrintNumbers);
+
+            firstThread.Start();
+            firstThread.Join();
+
+            Thread.Sleep(1000);
+
+            secondThread.Start();
+            secondThread.Join();
+        }
+
+        static void PrintNumbers()
+        {
+            for (int i = 1; i <= 100; i++)
+            {
+                Console.WriteLine(i);
+                Thread.Sleep(100);
+            }
         }
     }
 }
