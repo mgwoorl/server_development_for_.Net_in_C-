daa89010a037baa766b34f33ce8d988fb481651f
mgwoorl <149603747+mgwoorl@users.noreply.github.com>
2025-04-10 10:15:05 +0800

    Update Program.cs

--- Lab_1_1/Program.cs
+++ Lab_1_1/Program.cs
@@ -1,1 +1,29 @@
+using System;
+using System.Threading;
 
+namespace Lab_1_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Thread thread1 = new Thread(() => PrintNumbersInCertainRange(1, 10));
+            Thread thread2 = new Thread(() => PrintNumbersInCertainRange(11, 20));
+
+            thread1.Start();
+            thread2.Start();
+
+            thread1.Join();
+            thread2.Join();
+        }
+
+        static void PrintNumbersInCertainRange(int start, int end)
+        {
+            for (int i = start; i <= end; i++)
+            {
+                Console.WriteLine($"Поток {Thread.CurrentThread.ManagedThreadId}: {i}");
+                Thread.Sleep(100);
+            }
+        }
+    }
+}
